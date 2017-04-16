4efaaee045be80d8a99ae606e589ac23a691ba8e
Qingnan Zhou <qzhou@adobe.com>
2017-04-16 16:51:53 -0700

    Minor update.

--- include/igl/readPLY.cpp
+++ include/igl/readPLY.cpp
@@ -21,8 +21,13 @@ TEST(readPLY, cube){
 TEST(readPLY, two_boxes_bad_self_union){
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
-    igl::readPLY(test_common::data_path("two-boxes-bad-self-union.ply"), V, F);
+    for (size_t i=0; i<100; i++) {
+        igl::readPLY(test_common::data_path("two-boxes-bad-self-union.ply"), V, F);
+        ASSERT_EQ(486, V.rows());
+        ASSERT_EQ(708, F.rows());
 
-    ASSERT_EQ(486, V.rows());
-    ASSERT_EQ(708, F.rows());
+        igl::readPLY(test_common::data_path("cube.ply"), V, F);
+        ASSERT_EQ(8, V.rows());
+        ASSERT_EQ(12, F.rows());
+    }
 }
